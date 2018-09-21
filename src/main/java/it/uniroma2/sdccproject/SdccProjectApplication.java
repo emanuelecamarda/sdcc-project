fac2d1558ae459a52c8b3415772805f3b918be95
emanuele <emanuelecamarda@gmail.com>
2018-09-21 13:37:57 +0200

    Initial commit

---
+++ src/main/java/it/uniroma2/sdccproject/SdccProjectApplication.java
@@ -0,0 +1,12 @@
+package it.uniroma2.sdccproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdccProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdccProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/it/uniroma2/sdccproject/SdccProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package it.uniroma2.sdccproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SdccProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
